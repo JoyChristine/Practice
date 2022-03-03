052babf4162af452da23632dca30af2ec367743f
Joy Christine <joychristin2@gmail.com>
2022-03-03 12:49:16 +0300

    initial commit

---
+++ Practice/practice.js
@@ -0,0 +1,44 @@
+//all <input> in html shld be required
+// button - onclick="A_function_in JS"
+
+
+//create array of akan names
+// const maleNames = ["Kwasi", "Kwadwo","Kwabena"," Kwaku"," Yaw"," Kofi"," Kwame"]
+// const femaleNames = ["Akosua", "Adwoa","Abenaa","Akua","Yaa","Afua"," Ama"]
+
+
+//calculate day of the week using given formula
+//Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+//  CC - is the century digits. For example 1989 has CC = 19 | CC =parseInt(year.slice(0,2))
+//  YY - is the Year digits (1989 has YY = 89)               
+// YY =parseInt(year.slice(2,4)); or  
+//var YY = document.getElementById("#year").value;
+//YY = YY.slice (2,4);
+//  MM -  is the Month
+//  DD - is the Day of the month 
+//  mod - is the modulus function ( % )
+
+
+//  function formula () {
+//  dayOfTheWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )  % 7;
+//  CC = CC =parseInt(year.slice(0,2));
+//  YY =parseInt(year.slice(2,4)); || var YY = document.getElementById("#id_of_year_in_html").value; YY = YY.slice (2,4);
+//  MM = document.getElementById("#id_of_month_in_html").value;
+//  DD = document.getElementById(#id_of_day_in html").value;
+//  console.log(dayOfTheWeek);
+//  return (Math.floor(dayOfTheWeek));
+//  }
+
+
+
+
+// An invalid day should be (d<=0) or (d>31)
+//An invalid month should be (m<= 0) or (m > 12)  
+
+//the submit button;
+//document.getelementbyid("ID_OF_BUTTON");
+//addEventListener("click",Function_of_calculated_akan_name)
+//
+
+
+//output should be based on gender
